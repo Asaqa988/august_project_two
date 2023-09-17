fa8a8bee863d651f8374e1c8972fadf4ca1abc9f
asaqa988 <asaqa001@gmail.com>
2023-09-17 19:38:28 +0300

    update bas khaleto e3ml el language

--- cypress/e2e/myTest.cy.js
+++ cypress/e2e/myTest.cy.js
@@ -7,14 +7,25 @@ cy.on("uncaught:exception", (e) => {
 
 describe("template spec", () => {
   it("passes", () => {
+    let websites = [
+      "https://www.almosafer.com/ar",
+      "https://www.almosafer.com/en",
+    ];
 
-    let websites= ["https://www.almosafer.com/ar","https://www.almosafer.com/en"]
+    let RandomIndex = Math.floor(Math.random() * websites.length);
 
-    let RandomIndex = Math.floor(Math.random()*websites.length)
- 
-cy.visit(websites[RandomIndex])
+    cy.visit(websites[RandomIndex]);
 
-cy.get('.cta__saudi').click()
+    cy.get(".cta__saudi").click();
 
+    cy.get("#uncontrolled-tab-example-tab-hotels").click();
+
+    if (RandomIndex==0) {
+      cy.get('[data-testid="AutoCompleteInput"]').type("دبي")
+      
+    }else if (RandomIndex==1){
+      cy.get('[data-testid="AutoCompleteInput"]').type("dubai")
+
+    }
   });
 });
