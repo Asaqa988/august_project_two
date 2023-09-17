e9c2417a40b2c674b7e39ef4045626037a3b6369
asaqa988 <asaqa001@gmail.com>
2023-09-17 19:30:20 +0300

    update bas nset el test el awal akmlo

--- cypress/e2e/myTest.cy.js
+++ cypress/e2e/myTest.cy.js
@@ -1,5 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://global.almosafer.com/ar')
-  })
-})
+/// <reference types= "cypress" />
+
+
+cy.on("uncaught:exception", (e) => {
+  return false;
+});
+
+describe("template spec", () => {
+  it("passes", () => {
+
+    let websites= ["https://www.almosafer.com/ar","https://www.almosafer.com/en"]
+
+    let RandomIndex = Math.floor(Math.random()*websites.length)
+ 
+cy.visit(websites[RandomIndex])
+
+cy.get('.cta__saudi').click()
+
+  });
+});
